d934e9e0c9c4e58db1d54f35cf2e3a94d0986efd
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-21 13:12:09 -0800

    json

--- backend/batteryLog/src/server.js
+++ backend/batteryLog/src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 Object.entries(require("./requests.js")).forEach(method => {
     const requestMethod = app[method[0]].bind(app);
     const requests = method[1];
