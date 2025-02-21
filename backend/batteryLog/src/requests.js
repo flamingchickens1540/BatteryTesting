849934e0efc00f7e6d82149216547029d3637dd4
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-21 12:35:09 -0800

    Requests

--- backend/batteryLog/src/requests.js
+++ backend/batteryLog/src/requests.js
@@ -1,9 +1,25 @@
 const database = require("./database.js");
 
 module.exports = {
-    "/battery" : function(req, res) {
-        const query = req.query;
+    get : {
+        "/battery" : function(req, res) {
+            const query = req.query;
 
-        database.query(`SELECT * FROM batteries WHERE id=${query["battery-id"]}`, (error, result, fields) => res.send(result));
+            database.query(`SELECT * FROM batteries WHERE id=${query["battery-id"]};`, (error, result, fields) => res.send(result[0]));
+        },
+        "/batteryNames" : function(req, res) {
+            database.query(`SELECT name FROM batteries;`, (error, result, fields) => res.send(result.map(data => data.name)));
+        }
+    },
+    post : {
+        "/battery" : function(req, res) {
+            const query = req.query;
+
+            const body = req.body;
+
+            console.log(body);
+
+            database.query(`SELECT * FROM batteries WHERE id=${query["battery-id"]};`, (error, result, fields) => res.send(result[0]));
+        }
     }
 }
--- backend/batteryLog/src/server.js
+++ backend/batteryLog/src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-Object.entries(require("./requests.js")).forEach(entry => {
-    app.get(entry[0], entry[1]);
+Object.entries(require("./requests.js")).forEach(method => {
+    const requestMethod = app[method[0]];
+    const requests = method[1];
+    
+    Object.entries(requests).forEach(request => {
+        requestMethod(request[0], request[1]);
+    });
 });
 
 app.listen(process.env.PORT);
