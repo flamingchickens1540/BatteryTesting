681870cc32cecf78b251cbddca470efe7f06d452
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-21 14:14:57 -0800

    added await

--- backend/batteryLog/src/server.js
+++ backend/batteryLog/src/server.js
@@ -8,8 +8,8 @@ Object.entries(require("./requests.js")).forEach(method => {
     const requests = method[1];
     
     Object.entries(requests).forEach(request => {
-        requestMethod(request[0], (req, res) => {
-            const result = request[1](req);
+        requestMethod(request[0], async (req, res) => {
+            const result = await request[1](req);
 
             if(typeof result == Error) {
                 res.status(500);
