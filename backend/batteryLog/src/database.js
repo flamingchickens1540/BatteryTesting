f5d86e4b34415563d1f8640c2cfee64ad03af1e7
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-21 18:08:07 -0800

    bug fixes

--- backend/batteryLog/src/database.js
+++ backend/batteryLog/src/database.js
@@ -11,7 +11,7 @@ connection.connect();
  
 module.exports = {
   query : function(query, success) {
-    return new Promise(res => database.query(query, (error, result) => {
+    return new Promise(res => connection.query(query, (error, result) => {
       if(error) return res(error);
 
       res(success(result));
