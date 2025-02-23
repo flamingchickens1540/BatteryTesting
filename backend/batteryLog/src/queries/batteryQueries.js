30ee545fc2c528459085f7652275bc38bb7439f1
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-23 13:22:27 -0800

    fixed typo

--- backend/batteryLog/src/queries/batteryQueries.js
+++ backend/batteryLog/src/queries/batteryQueries.js
@@ -21,7 +21,7 @@ function removeBattery(id) {
 }
 
 function getBatteries() {
-    return database.execute(`SELECT id, name, capacity, date FROM ${BATTERIES_TABLES};`, result => ({batteries : result, length : result.length}));
+    return database.execute(`SELECT id, name, capacity, date FROM ${BATTERIES_TABLES};`, [], result => ({batteries : result, length : result.length}));
     // return database.query(`SELECT id, name, date FROM ${BATTERIES_TABLES};`, result => JSON.stringify({batteries : result, length : result.length}));
 }
 
