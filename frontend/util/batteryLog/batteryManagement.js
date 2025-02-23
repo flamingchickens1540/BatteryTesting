e72ae05ee57ecdaf9fc75995fa72e48a70e9c351
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-23 12:26:32 -0800

    made add battery not async

--- frontend/util/batteryLog/batteryManagement.js
+++ frontend/util/batteryLog/batteryManagement.js
@@ -11,14 +11,12 @@
         return fetch(`/BatteryTestingAPI/battery/remove/?battery-id=${_currentBattery.id}`, {method:"PUT"}).then(() => removeBattery(_currentBattery.id));
     }
 
-    async function addBatteryProfile(name, date, description) {
-        const battery = await fetch("/BatteryTestingAPI/battery/", {method:"PUT", mode:"cors", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
+    function addBatteryProfile(name, date, description) {
+        return fetch("/BatteryTestingAPI/battery/", {method:"PUT", mode:"cors", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
             batteryName : name,
             batteryDate : date,
             batteryDescription : description
-        })});
-
-        addBattery(battery);
+        })}).then(addBattery);
     }
 
     function getBattery() {
