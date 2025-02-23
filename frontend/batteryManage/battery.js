e87c3fdb49befe3fb428552a4354520205577927
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-23 12:33:49 -0800

    bound to batteryNamesElement

--- frontend/batteryManage/battery.js
+++ frontend/batteryManage/battery.js
@@ -8,7 +8,7 @@
     function fillBatteryList() {
         const batteryNamesElement = document.querySelector("#select .list");
 
-        batteryNamesElement.childNodes.forEach(batteryNamesElement.removeChild);
+        batteryNamesElement.childNodes.forEach(batteryNamesElement.removeChild, batteryNamesElement);
 
         getBatteries().map(battery => {
             const element = document.createElement("option");
@@ -44,6 +44,7 @@
 
     const addBattery = function() {
         addBatteryProfile(document.querySelector("#info #name input").value, document.querySelector("#info #date input").value, document.querySelector("#info #description textarea").value);
+        fillBatteryList();
     }
 
     document.querySelector("#select .list").addEventListener("change", event => switchBattery(event.target.value));
