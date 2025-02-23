2414380d465fcf9d3f843b47dae02240e130821d
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-23 15:14:59 -0800

    full deprecation of query

--- backend/batteryLog/src/queries/testQueries.js
+++ backend/batteryLog/src/queries/testQueries.js
@@ -5,30 +5,28 @@ const TIMESTAMPS_TABLE = "Timestamps";
 const CODE_VERSION = 0;
 
 function getBatteryTests(batteryId) {
-    return database.query(`SELECT * FROM ${TESTS_TABLE} WHERE batteryId=${Number(batteryId)};`, result => ({tests : result, length : result.length}));
+    return database.execute(`SELECT * FROM ${TESTS_TABLE} WHERE batteryId=?};`, [batteryId], result => ({tests : result, length : result.length}));
 }
 
 function getTest(testId) {
-    return database.query(`SELECT batteryId, startTime, name, success, capacity, codeVersion, startVoltage, duration, (SELECT MIN(voltage) FROM ${TIMESTAMPS_TABLE} WHERE testId=${Number(testId)}) AS minVoltage, (SELECT MAX(voltage) FROM ${TIMESTAMPS_TABLE} WHERE testId=${Number(testId)}) AS maxVoltage, (SELECT MIN(current) FROM ${TIMESTAMPS_TABLE} WHERE testId=${Number(testId)}) AS minCurrent, (SELECT MAX(current) FROM ${TIMESTAMPS_TABLE} WHERE testId=${Number(testId)}) AS maxCurrent FROM ${TESTS_TABLE} WHERE startTime=${Number(testId)};`, result => result[0]);
+    return database.execute(`SELECT batteryId, startTime, name, success, capacity, codeVersion, startVoltage, duration, (SELECT MIN(voltage) FROM ${TIMESTAMPS_TABLE} WHERE testId=?) AS minVoltage, (SELECT MAX(voltage) FROM ${TIMESTAMPS_TABLE} WHERE testId=?) AS maxVoltage, (SELECT MIN(current) FROM ${TIMESTAMPS_TABLE} WHERE testId=?) AS minCurrent, (SELECT MAX(current) FROM ${TIMESTAMPS_TABLE} WHERE testId=?) AS maxCurrent FROM ${TESTS_TABLE} WHERE startTime=?;`, [testId, testId, testId, testId, testId], result => result[0]);
 }
 
 function getTimestamps(testId) {
-    return database.query(`SELECT * FROM ${TIMESTAMPS_TABLE} WHERE testId=${Number(testId)};`, result => ({timestamps : result, length : result.length}));
+    return database.execute(`SELECT * FROM ${TIMESTAMPS_TABLE} WHERE testId=?;`, [testId], result => ({timestamps : result, length : result.length}));
 }
 
 function insertTimestamp(testId, time, voltage, current) {
-    return database.query(`INSERT INTO ${TIMESTAMPS_TABLE} VALUES(${Number(testId)}, ${Number(time)}, ${Number(voltage)}, ${Number(current)})`, () => time);
+    return database.query(`INSERT INTO ${TIMESTAMPS_TABLE} VALUES(?, ?, ?, ?)`, [testId, time, voltage, current], () => time);
 }
 
 async function logTest(batteryId, time, name, startVoltage, success, timestamps) {
-    if(isNaN(Number(batteryId)) || isNaN(Number(time)) || typeof name != "string")
-        return Error("Invalid Data");
+    // if(isNaN(Number(batteryId)) || isNaN(Number(time)) || typeof name != "string")
+    //     return Error("Invalid Data");
 
     const capacity = timestamps.map(timestamps => timestamps.current * timestamps.voltage).reduce((total, watt) => total + watt) / 60 / 60 / 1000;
 
-    time = Number(time);
-
-    await database.query(`INSERT INTO ${TESTS_TABLE} VALUES(${Number(batteryId)}, ${time}, ${timestamps[timestamps.length-1].time - time}, "${name.replaceAll('"', '')}", ${Number(startVoltage)}, ${capacity}, ${success ? 1 : 0}, ${CODE_VERSION});`, () => {});
+    await database.execute(`INSERT INTO ${TESTS_TABLE} VALUES(?, ?, ?, ?, ?, ?, ?, ?);`, [batteryId, time, timestamps[timestamps.length-1].time - time, name.replaceAll('"', ''), startVoltage, capacity, success ? 1 : 0, CODE_VERSION], () => {});
 
     for(const timestamp of timestamps) {
         const result = await insertTimestamp(time, Number(timestamp.time), Number(timestamp.voltage), Number(timestamp.current));
