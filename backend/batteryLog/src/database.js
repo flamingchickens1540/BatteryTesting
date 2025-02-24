871270e449994eefcdd5549a008416606bfd3250
brelee2222 <brelee1842@outlook.com>
2025-02-23 17:49:07 -0800

    updated env

--- backend/batteryLog/src/database.js
+++ backend/batteryLog/src/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host : "localhost",
+  host : process.env.HOST,
   user : process.env.USER,
   password : process.env.PASSWORD,
-  database : process.env.DATABASE
+  database : process.env.DATABASE,
+  path : process.env.PATH
 });
  
 connection.connect();
