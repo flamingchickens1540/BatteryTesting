c46dac0f305f5fb8a4f4f9a466df42d98143d47b
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-24 16:25:25 -0800

    asg

--- backend/batteryLog/src/queries/testQueries.js
+++ backend/batteryLog/src/queries/testQueries.js
@@ -44,7 +44,7 @@ async function logTest(batteryId, time, name, startVoltage, success, timestamps)
 
     const capacity = timestamps.map(timestamp => timestamp.current * timestamp.voltage * timestamp.time).reduce((total, watt) => total + watt) / 60 / 60 / 1000;
 
-    await database.execute(`INSERT INTO ${TESTS_TABLE} VALUES(?, ?, ?, ?, ?, ?, ?, ?);`, [batteryId, time, timestamps[timestamps.length-1].time - time, name.replaceAll('"', ''), startVoltage, capacity, success ? 1 : 0, CODE_VERSION], () => {});
+    await database.execute(`INSERT INTO ${TESTS_TABLE} (batteryId, startTime, duration, name, startVoltage, capacity, success, codeVersion) VALUES(?, ?, ?, ?, ?, ?, ?, ?);`, [batteryId, time, timestamps[timestamps.length-1].time - time, name.replaceAll('"', ''), startVoltage, capacity, success ? 1 : 0, CODE_VERSION], () => {});
 
     for(const timestamp of timestamps) {
         const result = await insertTimestamp(time, Number(timestamp.time), Number(timestamp.voltage), Number(timestamp.current));
