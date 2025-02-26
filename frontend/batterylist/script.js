b4c3c996a68592c250b171e6bd8b0dc53d8edc44
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-26 11:45:20 -0800

    battery list

---
+++ frontend/batterylist/script.js
@@ -0,0 +1,35 @@
+{
+    (async function() {
+        await batteryInit;
+
+        fillBatteryList();
+    })();
+
+    const fillBatteryList = function() {
+        const batteryListElement = document.querySelector("#batteryList");
+
+        while(batteryListElement.children[0])
+            batteryListElement.removeChild(batteryListElement[0]);
+
+        getBatteries().forEach(battery => {
+            const batteryItemElement = document.createElement("div");
+
+            batteryItemElement.className = "batteryItem";
+            batteryItemElement.id = battery.id;
+
+            const nameElement = document.createElement("span");
+            nameElement.innerText = battery.name;
+            batteryItemElement.appendChild(nameElement);
+
+            const startVoltageElement = document.createElement("span");
+            startVoltageElement.innerText = battery.startVoltage;
+            batteryItemElement.appendChild(startVoltageElement);
+
+            const capacityElement = document.createElement("span");
+            capacityElement.innerText = battery.capacity;
+            batteryItemElement.appendChild(capacityElement);
+
+            batteryListElement.appendChild(batteryItemElement);
+        })
+    }
+}
