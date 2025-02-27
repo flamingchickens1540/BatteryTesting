398ad6dfb4b1aac89ed670fab0774fc42f64076d
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-27 09:57:44 -0800

    fixes

--- frontend/notesmanage/script.js
+++ frontend/notesmanage/script.js
@@ -1,8 +1,7 @@
 {
     const batteryId = new URLSearchParams(window.location.search).get("battery-id");
 
-    document.querySelector("#add").addEventListener("click", () => fetch("/BatteryTestingAPI/battery/", {method:"PUT", mode:"cors", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
-        batteryId,
+    document.querySelector("#add").addEventListener("click", () => fetch(`/BatteryTestingAPI/note/?battery-id=${batteryId}`, {method:"PUT", mode:"cors", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
         time : Date.now(),
         note : document.querySelector("#note").value
     })}));
