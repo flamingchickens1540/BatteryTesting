ba9e1efac77e3629e40d37cb0d7503e1e89a5589
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-28 14:50:49 -0800

    resize

--- frontend/batterylist/notes.js
+++ frontend/batterylist/notes.js
@@ -25,6 +25,6 @@
 
     document.querySelector("#manageNotes").addEventListener("click", () => {
         if(getBattery())
-            window.open(`notesmanage/`, "Manage NotesWindow");
+            window.open(`notesmanage/`, "Manage NotesWindow", "width=1200,height=600");
     });
 }
