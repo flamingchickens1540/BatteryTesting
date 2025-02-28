6fd82769650d016fa1ba870e0385e480d9526639
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2025-02-27 16:54:29 -0800

    mobile test

--- frontend/notesmanage/script.js
+++ frontend/notesmanage/script.js
@@ -1,6 +1,8 @@
 {
     const batteryId = new URLSearchParams(window.location.search).get("battery-id");
 
+    alert(window.opener.getBattery().name);
+
     document.querySelector("#add").addEventListener("click", () => fetch(`/BatteryTestingAPI/note/?battery-id=${batteryId}`, {method:"PUT", mode:"cors", headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
         time : Date.now(),
         note : document.querySelector("#note").value
